93fa5c92b483d23ffc661772788ad4191cecd515
Kamil Korzekwa <kamkor@gmail.com>
2015-09-19 12:23:31 +0200

    Refactor Java covariant array example

--- src/test/java/kamkor/covariance/CovariantArrayExample.java
+++ src/test/java/kamkor/covariance/CovariantArrayExample.java
@@ -7,12 +7,12 @@ public class CovariantArrayExample {
 	// Scala solves this by making its Arrays invariant (nonvariant)
 	@Test(expected = ArrayStoreException.class)
 	public void covariantArraysLeadToRuntimeExceptions() {
-		String[] c1 = { "abc" };
-		Object[] c2 = c1;
+		String[] strings = { "abc" };
+		Object[] objects = strings;
 		// OOPS! Line below throws a runtime exception: ArrayStoreException
-		// Reason is, that c2 is actually an instance of String array, and
+		// Reason is, that objects is actually an instance of String array, and
 		// we try to update it with an Integer.
-		c2[0] = 1;
+		objects[0] = 1;
 	}
 
 }

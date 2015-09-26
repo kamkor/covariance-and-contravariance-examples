c30606c513fcf9a9fdba074dbe7063a86759b17e
Kamil Korzekwa <kamkor@gmail.com>
2015-09-26 14:12:27 +0200

    Refactor AmmoMagazine

--- src/main/scala/kamkor/covariance/gun/AmmoMagazine.scala
+++ src/main/scala/kamkor/covariance/gun/AmmoMagazine.scala
@@ -4,20 +4,19 @@ import kamkor.covariance.gun.BulletsModel._
 
 object AmmoMagazine {
   def newNormalBulletsMag: AmmoMagazine[NormalBullet] = {
-    val bullets = List.range(0, 10) map (i => new NormalBullet)
+    val bullets = List.range(0, 10) map (_ => new NormalBullet)
     new AmmoMagazine(bullets)
   }
 
   def newExplosiveBulletsMag: AmmoMagazine[ExplosiveBullet] = {
-    val explosiveBullets = List.range(0, 10) map (i => new ExplosiveBullet)
+    val explosiveBullets = List.range(0, 10) map (_ => new ExplosiveBullet)
     new AmmoMagazine(explosiveBullets)
   }
 }
 
 /*
- * Covariant type parameter used in a mutable field, works only because
- * of private[this] scope. If you remove [this], code below will not
- * compile.
+ * Covariant type parameter used in a mutable field. Compiles only because
+ * of private[this] scope. If you remove [this], code below will not compile.
  */
 final class AmmoMagazine[+A <: Bullet](private[this] var bullets: List[A]) {
 

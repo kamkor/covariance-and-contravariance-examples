cf03b274eddc4bec70500da85d6a96074ba84b1c
Kamil Korzekwa <kamkor@gmail.com>
2015-12-01 23:55:59 +0100

    remove implementation

--- src/main/scala/kamkor/contravariance/garbagecan/GarbageCan.scala
+++ src/main/scala/kamkor/contravariance/garbagecan/GarbageCan.scala
@@ -2,16 +2,7 @@ package kamkor.contravariance.garbagecan
 
 import kamkor.contravariance.garbagecan.TrashModel._
 
-class GarbageCan[-A] {
-
-  // compiles because of object private scope
-  private[this] var items: List[A] = List.empty
-
-  def put(item: A): Unit = this.items :+= item
-
-  def putAll(items: List[A]): Unit = this.items ++= items
-
-  def itemsCount: Int = this.items.size
+class GarbageCan[A] {
 
 }
 
@@ -23,22 +14,22 @@ object GarbageCanExamples extends App {
     }
   }
 
-  def contravariantSubtyping: Unit = {
-    val frontYard = new FrontYard
+//  def contravariantSubtyping: Unit = {
+//    val frontYard = new FrontYard
+//
+//    // contravariant subtyping
+//    val itemGC: GarbageCan[Item] = new GarbageCan
+//    frontYard.setGarbageCan(itemGC)
+//
+//    // invariant
+//    val plasticItemGC: GarbageCan[PlasticItem] = new GarbageCan
+//    frontYard.setGarbageCan(plasticItemGC)
+//
+//    // covariant subtyping, won't compile
+//    val plasticBottleGC: GarbageCan[PlasticBottle] = new GarbageCan
+//    //frontYard.setGarbageCan(plasticBottleGC)
+//  }
 
-    // contravariant subtyping
-    val itemGC: GarbageCan[Item] = new GarbageCan
-    frontYard.setGarbageCan(itemGC)
-
-    // invariant
-    val plasticItemGC: GarbageCan[PlasticItem] = new GarbageCan
-    frontYard.setGarbageCan(plasticItemGC)
-
-    // covariant subtyping, won't compile
-    val plasticBottleGC: GarbageCan[PlasticBottle] = new GarbageCan
-    //frontYard.setGarbageCan(plasticBottleGC)
-  }
-
-  contravariantSubtyping
+//  contravariantSubtyping
 
 }
--- src/main/scala/kamkor/covariance/vendingmachine/VendingMachine.scala
+++ src/main/scala/kamkor/covariance/vendingmachine/VendingMachine.scala
@@ -2,16 +2,7 @@ package kamkor.covariance.vendingmachine
 
 import kamkor.covariance.vendingmachine.DrinksModel._
 
-class VendingMachine[+A](val currentItem: Option[A], items: List[A]) {
-
-  def this(items: List[A]) = this(None, items)
-
-  def dispenseNext: VendingMachine[A] = items match {
-    case Nil => if (currentItem.isDefined) new VendingMachine(None, Nil) else this
-    case t :: ts => new VendingMachine(Some(t), ts)
-  }
-
-  def addAll[B >: A](newItems: List[B]): VendingMachine[B] = new VendingMachine(items ++ newItems)
+class VendingMachine[A]() {
 
 }
 
@@ -22,40 +13,40 @@ object VendingMachineExamples extends App {
     def installVendingMachine(vm: VendingMachine[SoftDrink]): Unit = {}
   }
 
-  def covariantSubtyping: Unit = {
-    val cafeteria = new Cafeteria
+//  def covariantSubtyping: Unit = {
+//    val cafeteria = new Cafeteria
+//
+//    // covariant subtyping
+//    val colaVM: VendingMachine[Cola] = new VendingMachine(List(new Cola))
+//    cafeteria.installVendingMachine(colaVM)
+//
+//    // invariant
+//    val softDrinkVM: VendingMachine[SoftDrink] = new VendingMachine(List(new Cola, new TonicWater))
+//    cafeteria.installVendingMachine(softDrinkVM)
+//
+//    // contravariant subtyping, won't compile
+//    val juiceVM: VendingMachine[Juice] = new VendingMachine(List(new AppleJuice, new OrangeJuice))
+//    //cafeteria.installVendingMachine(juiceVM)
+//  }
+//
+//  def vendingMachineIsImmutable: Unit = {
+//    // vending machine is immutable !
+//    var vm: VendingMachine[Cola] = new VendingMachine(List(new Cola))
+//    vm = vm.dispenseNext
+//    vm.currentItem.foreach(println) // prints cola
+//    println(vm.dispenseNext.currentItem.isEmpty) // prints true
+//  }
+//
+//  def lowerBoundCanBePowerfull: Unit = {
+//    val colaVM: VendingMachine[Cola] = new VendingMachine(List(new Cola))
+//    val tonics: List[TonicWater] = List(new TonicWater)
+//
+//    // type changed to more generic
+//    val softDrinkVM: VendingMachine[SoftDrink] = colaVM.addAll(tonics)
+//  }
 
-    // covariant subtyping
-    val colaVM: VendingMachine[Cola] = new VendingMachine(List(new Cola))
-    cafeteria.installVendingMachine(colaVM)
-
-    // invariant
-    val softDrinkVM: VendingMachine[SoftDrink] = new VendingMachine(List(new Cola, new TonicWater))
-    cafeteria.installVendingMachine(softDrinkVM)
-
-    // contravariant subtyping, won't compile
-    val juiceVM: VendingMachine[Juice] = new VendingMachine(List(new AppleJuice, new OrangeJuice))
-    //cafeteria.installVendingMachine(juiceVM)
-  }
-
-  def vendingMachineIsImmutable: Unit = {
-    // vending machine is immutable !
-    var vm: VendingMachine[Cola] = new VendingMachine(List(new Cola))
-    vm = vm.dispenseNext
-    vm.currentItem.foreach(println) // prints cola
-    println(vm.dispenseNext.currentItem.isEmpty) // prints true
-  }
-
-  def lowerBoundCanBePowerfull: Unit = {
-    val colaVM: VendingMachine[Cola] = new VendingMachine(List(new Cola))
-    val tonics: List[TonicWater] = List(new TonicWater)
-
-    // type changed to more generic
-    val softDrinkVM: VendingMachine[SoftDrink] = colaVM.addAll(tonics)
-  }
-
-  covariantSubtyping
-  vendingMachineIsImmutable
-  lowerBoundCanBePowerfull
+//  covariantSubtyping
+//  vendingMachineIsImmutable
+//  lowerBoundCanBePowerfull
 
 }

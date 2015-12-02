527440516248ffbb86f23a61e86bb6221345a4c0
Kamil Korzekwa <kamkor@gmail.com>
2015-12-02 17:41:50 +0100

    clean up presentation template

--- src/main/scala/kamkor/contravariance/garbagecan/GarbageCan.scala
+++ src/main/scala/kamkor/contravariance/garbagecan/GarbageCan.scala
@@ -6,30 +6,12 @@ class GarbageCan[A] {
 
 }
 
-object GarbageCanExamples extends App {
+class FrontYard {
+  def setGarbageCan(plasticItemGC: GarbageCan[PlasticItem]): Unit = {
 
-  class FrontYard {
-    def setGarbageCan(plasticItemGC: GarbageCan[PlasticItem]): Unit = {
-
-    }
   }
+}
 
-//  def contravariantSubtyping: Unit = {
-//    val frontYard = new FrontYard
-//
-//    // contravariant subtyping
-//    val itemGC: GarbageCan[Item] = new GarbageCan
-//    frontYard.setGarbageCan(itemGC)
-//
-//    // invariant
-//    val plasticItemGC: GarbageCan[PlasticItem] = new GarbageCan
-//    frontYard.setGarbageCan(plasticItemGC)
-//
-//    // covariant subtyping, won't compile
-//    val plasticBottleGC: GarbageCan[PlasticBottle] = new GarbageCan
-//    //frontYard.setGarbageCan(plasticBottleGC)
-//  }
-
-//  contravariantSubtyping
+object GarbageCanExamples extends App {
 
 }
